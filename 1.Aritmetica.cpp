81cc586e1e55a79e8fa72d5dc9611dadbacf5c6c
dafloresdp <149301894+dafloresdp@users.noreply.github.com>
2023-12-10 23:59:54 +0100

    Update 1.Aritmetica.cpp

--- 1.Aritmetica.cpp
+++ 1.Aritmetica.cpp
@@ -3,50 +3,68 @@
 using namespace std;
 
 int main() {
+
+    double num1, num2, resultat;
     cout << "Has seleccionat la Calculadora aritmetica basica." << endl;
     cout << "Indica la teva operacio: suma (S), resta (R), multiplicacio (M), divisio (D) seguida dels 2 nombres a operar. Pots tancar aquesta funcio amb el commandament #." << endl;
 
-    bool error = false;
+    bool errorMostrado = false;
 
-    while (true) {
+    while (true)
+    {
         char operacio;
         cin >> operacio;
 
-        if (operacio == '#') {
+        if (operacio == '#')
+        {
             break;
         }
 
-        double num1, num2, resultat;
+        ;
 
-        if (operacio == 'S') {
+        if (operacio == 'S')
+        {
             cin >> num1 >> num2;
             resultat = num1 + num2;
-            cout << resultat << endl;
-            error = false;
-        } else if (operacio == 'R') {
+            cout << num1 << "+" << num2 << "= "<< resultat << endl;
+            errorMostrado = false;
+        }
+        else if (operacio == 'R')
+        {
             cin >> num1 >> num2;
             resultat = num1 - num2;
-            cout << resultat << endl;
-            error = false;
-        } else if (operacio == 'M') {
+            cout << num1 << "-" << num2 << "= " << resultat << endl;
+            errorMostrado = false;
+        }
+        else if (operacio == 'M')
+        {
             cin >> num1 >> num2;
             resultat = num1 * num2;
-            cout << resultat << endl;
-            error = false;
-        } else if (operacio == 'D') {
+            cout << num1 << "*" << num2 << "= " << resultat << endl;
+            errorMostrado = false;
+        }
+        else if (operacio == 'D')
+        {
             cin >> num1 >> num2;
-            if (num2 != 0) {
+            if (num2 != 0)
+            {
                 resultat = num1 / num2;
-                cout << resultat << endl;
-                error = false;
-            } else {
-                cout << "Error: Divisio per zero." << endl;
-                error = true;
+                cout << num1 << "/" << num2 << "= " << resultat << endl;
+                errorMostrado = false;
             }
-        } else {
-            if (!error) {
-                cout << "Error: Valor incorrecte. Si us plau, introdueix un valor valid. Escriu 'A' per veure les instruccions de nou." << endl;
-                error = true;
+            else
+            {
+                cout << "Error: Divisio per zero." << endl;
+                errorMostrado = true;
             }
         }
+        else
+        {
+            if (!errorMostrado)
+            {
+                cout << "Error: Valor incorrecte. Si us plau, introdueix un valor valid. Escriu 'A' per veure les instruccions de nou." << endl;
+                errorMostrado = true;
+            }
+        }
+    }
     }
