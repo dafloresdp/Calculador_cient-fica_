f8c14cd0a7de021dcb61b6c6b7d5825f51edf59b
dafloresdp <149301894+dafloresdp@users.noreply.github.com>
2023-12-11 00:05:02 +0100

    Update 1_incognita.cpp

--- 1_incognita.cpp
+++ 1_incognita.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     while (true) {
-        cout << "Calculadora Multifuncional: Indica la seg�ent operacio o escriu 'A' per veure les instruccions de nou." << endl;
+        cout << "Calculadora Multifuncional: Indica la següent operacio o escriu 'A' per veure les instruccions de nou." << endl;
 
         char opcio;
         cin >> opcio;
@@ -11,8 +11,8 @@ int main() {
         if (opcio == '#') {
             break;
         } else if (opcio == '5') {
-            cout << "Has seleccionat la Calculadora d�equacions lineals (1 incognita)" << endl;
-            cout << "Indica tots els termes de l�equacio, inclosa la igualtat (=) cadascuna en una linia nova. Quan hagis acabat d�escriure l�equacio, indica-ho amb un punt �.� Pots tancar aquesta funcio amb el comandament #." << endl;
+            cout << "Has seleccionat la Calculadora d’equacions lineals (1 incognita)" << endl;
+            cout << "Indica tots els termes de l’equacio, inclosa la igualtat (=) cadascuna en una linia nova. Quan hagis acabat d’escriure l’equacio, indica-ho amb un punt ‘.’ Pots tancar aquesta funcio amb el comandament #." << endl;
 
             double q = 0;  // Coeficient inicialitzat a 0
             double c = 0;  // Constant inicialitzada a 0
@@ -52,4 +52,66 @@ int main() {
 
     return 0;
 }
+
+
+
+/*INTENT NÚMERO 2
+    #include <iostream>
+    #include <stdlib>
+    #include <cmath>
+    
+    using namespace std;
+
+int main() {
+
+
+    cout << "Has seleccionat la Calculadora d’equacions lineals (1 incognita)" << endl;
+    cout << "Indica tots els termes de l’equacio, inclosa la igualtat (=) cadascuna en una linia nova. Quan hagis acabat d’escriure l’equacio, indica-ho amb un punt ‘.’ Pots tancar aquesta funcio amb el comandament #." << endl;
+
+    double q = 0;  // Coeficient inicialitzat a 0
+    double c = 0;  // Constant inicialitzada a 0
+    char input;
+
+    while (true)
+    {
+        cin >> input;
+
+
+        if (input == '.')
+        {
+            if (q == 0)
+            {
+                if (c == 0)
+                {
+                    cout << "Equacio amb infinites solucions." << endl;
+                }
+                else
+                {
+                    cout << "Equacio sense solucio." << endl;
+                }
+            }
+            else
+            {
+                cout << "Solucio: x = " << -c / q << endl;
+            }
+            break;
+        }
+        else if (input == '=')
+        {
+            cout << "x" << endl;
+        }
+        else if (input == 'x')
+        {
+            cin >> q;
+        }
+        else
+        {
+            cin.putback(input);
+            cin >> c;
+        }
+        while (q != '#');
+    }
+
+}
+
 
