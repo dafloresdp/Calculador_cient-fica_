0f71ff3d2811b3b9c1e4991ce2b05f65c48fb956
dafloresdp <149301894+dafloresdp@users.noreply.github.com>
2023-12-11 00:02:28 +0100

    Update arrels.cpp

--- arrels.cpp
+++ arrels.cpp
@@ -2,25 +2,39 @@ include <iostream>
 include <cmath>
 using namespace std;
 int main () {
-    cout << "\nHas seleccionat la Calculadora d'arrels." << endl;
+  
+     cout << "\nHas seleccionat la Calculadora d'arrels." << endl;
     cout << "Indica el radicand seguit de l'index. Pots tancar aquesta funcio amb el comandament #." << endl;
 
     while (true)
     {
         double radicand, index, resultat;
 
+
         // radicand i index
-        cout << ">> ";
+
         cin >> radicand;
         if (radicand == '#') break;
 
         cin >> index;
+        if (index == 2)
+        {
+            resultat = pow(radicand, 1 / index);
+            cout<< "L'arrel quadrada de " << radicand << " es: " << resultat << endl;
+
+        }
+        if (index == 3)
+        {
+            resultat = pow(radicand, 1 / index);
+            cout << "L'arrel cubica de " << radicand << " es: " << resultat << endl;
+        }
+
 
         if (radicand < 0 && ((int)index) % 2 == 0)
         {
             cerr << "Error: el radicand no pot ser negatiu." << endl;
         }
-        else
+        else if (index>=4)
         {
             resultat = pow(radicand, 1 / index);
             cout << "L'arrel " << index << "a de " << radicand << " es: " << resultat << endl;
@@ -28,3 +42,7 @@ int main () {
     }
 }
 
+
+
+
+    
