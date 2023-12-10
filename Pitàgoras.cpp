a386b58e21fe8a53ccb3924a3d6bc4e8d3c4a9c2
dafloresdp <149301894+dafloresdp@users.noreply.github.com>
2023-12-11 00:03:18 +0100

    Update Pitàgoras.cpp

--- Pitàgoras.cpp
+++ Pitàgoras.cpp
@@ -3,78 +3,50 @@
 #include <sstream>
 
 using namespace std;
+int main (){
 
-void calcularHipotenusa() {
-    cout << "Vols calcular la hipotenusa. Introdueix la mida dels catets separada per un espai." << endl;
-    cout << ">> ";
+ cout << "\nHas seleccionat el Teorema de Pitagores." << endl;
+    cout << "Indica que vols calcular: la mida del catet (C) o la de la hipotenusa (H). Seguidament, el programa et preguntara per les dades necessaries." << endl;
+    char opcio;
+    cin >> opcio;
 
-    double catet1, catet2;
-    cin >> catet1 >> catet2;
+    if (opcio == 'H' || opcio == 'h')
+    {
+        cout << "Vols calcular la hipotenusa. Introdueix la mida dels catets separada per un espai." << endl;
 
-    double hipotenusa = sqrt(pow(catet1, 2) + pow(catet2, 2));
+        double catet1, catet2;
+        cin >> catet1 >> catet2;
 
-    cout << "Catet1 = " << catet1 << ", Catet2 = " << catet2 << ", Hipotenusa = " << hipotenusa << endl;
-}
+        double hipotenusa = sqrt(pow(catet1, 2) + pow(catet2, 2));
 
-void calcularCatet() {
-    cout << "Vols calcular el catet. Introdueix la mida de l’altre catet i de la hipotenusa separades per un espai." << endl;
-    cout << ">> ";
+        cout << "Catet1 = " << catet1 << ", Catet2 = " << catet2 << ", Hipotenusa = " << hipotenusa << endl;
+    }
+    if (opcio == 'C' || opcio == 'c')
+    {
+        cout << "Vols calcular el catet. Introdueix la mida de lâ€™altre catet i de la hipotenusa separades per un espai." << endl;
 
-    double catet1, hipotenusa;
-    cin >> catet1 >> hipotenusa;
+        double catet1, hipotenusa;
+        cin >> catet1 >> hipotenusa;
 
-    double catet2 = sqrt(pow(hipotenusa, 2) - pow(catet1, 2));
+        double catet2 = sqrt(pow(hipotenusa, 2) - pow(catet1, 2));
 
-    cout << "Catet1 = " << catet1 << ", Catet2 = " << catet2 << ", Hipotenusa = " << hipotenusa << endl;
-}
+        cout << "Catet1 = " << catet1 << ", Catet2 = " << catet2 << ", Hipotenusa = " << hipotenusa << endl;
 
-int main() {
-    while (true) {
-        cout << "Calculadora Multifuncional: Indica la següent operació o escriu 'A' per veure les instruccions de nou." << endl;
-        cout << ">> ";
-
-        char opcio;
-        cin >> opcio;
-
-        if (opcio == '4') {
-            cout << "Has seleccionat el Teorema de Pitagores." << endl;
-            cout << "Indica que vols calcular: la mida del catet (C) o la de la hipotenusa (H). Seguidament, el programa et preguntara per les dades necessaries." << endl;
-
-            cout << ">> ";
-            cin >> opcio;
-
-            if (opcio == 'H' || opcio == 'h') {
-                cout << "Vols calcular la hipotenusa. Introdueix la mida dels catets separada per un espai." << endl;
-                cout << ">> ";
-
-                double catet1, catet2;
-                cin >> catet1 >> catet2;
-
-                double hipotenusa = sqrt(pow(catet1, 2) + pow(catet2, 2));
-
-                cout << "Catet1 = " << catet1 << ", Catet2 = " << catet2 << ", Hipotenusa = " << hipotenusa << endl;
-            } else if (opcio == 'C' || opcio == 'c') {
-                cout << "Vols calcular el catet. Introdueix la mida de l’altre catet i de la hipotenusa separades per un espai." << endl;
-                cout << ">> ";
-
-                double catet1, hipotenusa;
-                cin >> catet1 >> hipotenusa;
-
-                double catet2 = sqrt(pow(hipotenusa, 2) - pow(catet1, 2));
-
-                cout << "Catet1 = " << catet1 << ", Catet2 = " << catet2 << ", Hipotenusa = " << hipotenusa << endl;
-            } else {
-                cout << "Error: valor incorrecte. Indica que vols calcular: la mida del catet (C) o la de la hipotenusa (H). Seguidament, el programa et preguntara per les dades necessaries." << endl;
-            }
-        } else if (opcio == 'A' || opcio == 'a') {
-            // Muestra las instrucciones nuevamente
-            cout << "Instruccions: ..." << endl;
-        } else if (opcio == '#') {
-            break;
-        } else {
-            cout << "Error: Opcio incorrecta. Si us plau, introdueix una opcio valida." << endl;
         }
+        else if  (opcio == '#') {
+
+          cin.clear();
+    }
+    else
+    {
+        cerr << "Error: valor incorrecte. Indica que vols calcular: la mida del catet (C) o la de la hipotenusa (H). Seguidament, el programa et preguntara per les dades necessaries." << endl;
     }
 
-    return 0;
+
+
+
+
+
+    while (opcio!= '#');
 }
+
