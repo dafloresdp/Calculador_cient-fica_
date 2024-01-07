62ede3726a0f0c6dbd2a9936eb66e7561218ebd8
dafloresdp <149301894+dafloresdp@users.noreply.github.com>
2024-01-07 22:21:44 +0100

    Update 1_incognita.cpp

--- 1_incognita.cpp
+++ 1_incognita.cpp
@@ -1,117 +1,67 @@
 #include <iostream>
+#include <sstream>
+#include <string>
+
 using namespace std;
+int main()
+{
+    double qt_x = 0.0;
+    double ctt = 0.0;
+    bool right = false;
 
-int main() {
-    while (true) {
-        cout << "Calculadora Multifuncional: Indica la següent operacio o escriu 'A' per veure les instruccions de nou." << endl;
-
-        char opcio;
-        cin >> opcio;
-
-        if (opcio == '#') {
-            break;
-        } else if (opcio == '5') {
-            cout << "Has seleccionat la Calculadora d’equacions lineals (1 incognita)" << endl;
-            cout << "Indica tots els termes de l’equacio, inclosa la igualtat (=) cadascuna en una linia nova. Quan hagis acabat d’escriure l’equacio, indica-ho amb un punt ‘.’ Pots tancar aquesta funcio amb el comandament #." << endl;
-
-            double q = 0;  // Coeficient inicialitzat a 0
-            double c = 0;  // Constant inicialitzada a 0
-            char input;
-
-            while (true) {
-                cin >> input;
-
-                if (input == '#') {
-                    break;
-                } else if (input == '.') {
-                    if (q == 0) {
-                        if (c == 0) {
-                            cout << "Equacio amb infinites solucions." << endl;
-                        } else {
-                            cout << "Equacio sense solucio." << endl;
-                        }
-                    } else {
-                        cout << "Solucio: x = " << -c / q << endl;
-                    }
-                    break;
-                } else if (input == '=') {
-                    cout << "x" << endl;
-                } else if (input == 'x') {
-                    cin >> q;
-                } else {
-                    cin.putback(input);
-                    cin >> c;
-                }
-            }
-        } else {
-            cout << "Error: Valor incorrecte. Si us plau, introdueix un valor valid. Escriu 'A' per veure les instruccions de nou." << endl;
-        }
-    }
-
-    cout << "Fi del programa. Adeu i gracies!" << endl;
-
-    return 0;
-}
-
-
-
-/*INTENT NÚMERO 2
-    #include <iostream>
-    #include <stdlib>
-    #include <cmath>
-    
-    using namespace std;
-
-int main() {
-
-
-    cout << "Has seleccionat la Calculadora d’equacions lineals (1 incognita)" << endl;
-    cout << "Indica tots els termes de l’equacio, inclosa la igualtat (=) cadascuna en una linia nova. Quan hagis acabat d’escriure l’equacio, indica-ho amb un punt ‘.’ Pots tancar aquesta funcio amb el comandament #." << endl;
-
-    double q = 0;  // Coeficient inicialitzat a 0
-    double c = 0;  // Constant inicialitzada a 0
-    char input;
-
-    while (true)
+    string entrada;
+    while (getline(cin, entrada) && entrada != "." && entrada != "#" && entrada != "A")
     {
-        cin >> input;
+        istringstream stream(entrada);
 
-
-        if (input == '.')
+        string terme;
+        while (stream >> terme)
         {
-            if (q == 0)
+            if (terme == "x")
             {
-                if (c == 0)
-                {
-                    cout << "Equacio amb infinites solucions." << endl;
-                }
-                else
-                {
-                    cout << "Equacio sense solucio." << endl;
-                }
+                qt_x += (right? -1.0 : 1.0);
+            }
+            else if (terme == "=")
+            {
+                right = true;
             }
             else
             {
-                cout << "Solucio: x = " << -c / q << endl;
+                size_t pos;
+                double qt_term;
+                if ((pos = terme.find('x')) != string::npos)
+                {
+                    string coef_str = terme.substr(0, pos);
+                    stringstream term_ss(coef_str);
+                    if (term_ss >> qt_term)
+                    {
+                        qt_x += (right ? -1.0 : 1.0) * qt_term;
+                    }
+                    else
+                    {
+                        cout << "Error: Valor incorrecte. Si us plau, introdueix un valor valid. Escriu 'A' per veure les instruccions de nou." << terme << endl;
+                        return 1;
+                    }
+                }
+                else
+                {
+                    stringstream term_ss(terme);
+                    if (term_ss >> qt_term)
+                    {
+                        ctt += (right ? -1.0 : 1.0) * qt_term;
+                    }
+                    else
+                    {
+                        cout << "Error: Valor incorrecte. Si us plau, introdueix un valor valid. Escriu 'A' per veure les instruccions de nou." << terme << endl;
+                        return 1;
+                    }
+                }
             }
-            break;
         }
-        else if (input == '=')
-        {
-            cout << "x" << endl;
-        }
-        else if (input == 'x')
-        {
-            cin >> q;
-        }
-        else
-        {
-            cin.putback(input);
-            cin >> c;
-        }
-        while (q != '#');
     }
 
-}
+    cout << "El terme 'x' es igual a: " << ctt / qt_x << endl;
 
+    return 0;
+}
 
